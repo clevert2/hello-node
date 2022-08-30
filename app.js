20a15b660f953aec0b8cc9a12357fe0b91ec9e63
clevert2 <chad.levert@va.gov>
2022-08-30 19:36:54 +0000

    add echo

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send(`Hello ${target}!`);
 });
 
+app.get('/test', (req, res) => {
+  console.log('Hitting test endpoint.');
+
+  const mymessage = req.query.message;
+  res.send(mymessage);
+  res.send(`Test endpoint - Hello world.    `);
+});
+
 const port = process.env.PORT || 8080;
 module.exports = app.listen(port, () => {
   console.log('Hello world listening on port', port);
